49da11b831d752f41b6836ed19ea2011b60e4ea9
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-02-24 08:59:38 +0100

    Bump to 0.0.2 version

--- info.el
+++ info.el
@@ -1,3 +1,3 @@
 (live-pack-name "lisp-pack")
-(live-pack-version "0.0.1")
-(live-pack-description "lisp-pack - some lisp specific setup")
+(live-pack-version "0.0.2")
+(live-pack-description "lisp-pack - Lisp specific setup")
