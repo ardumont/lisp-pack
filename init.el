6f253a6bacf5380f539d0c679145972fc36e2922
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-01 19:26:09 +0100

    C-c e - eval-and-replace

--- init.el
+++ init.el
@@ -44,6 +44,8 @@
 (global-set-key (kbd "C-c s h") 'fold-dwim-hide-all)
 (global-set-key (kbd "C-c s s") 'fold-dwim-show-all)
 
+(global-set-key (kbd "C-c e") 'eval-and-replace)
+
 (define-key paredit-mode-map (kbd "C-h") 'paredit-backward-delete)
 
 ;;; lisp-pack ends here
