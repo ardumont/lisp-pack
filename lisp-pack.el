136309a1ca586f07cf790b6ebce5a1339de63934
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-30 20:17:10 +0200

    Refactor - use-package

--- lisp-pack.el
+++ lisp-pack.el
@@ -4,21 +4,15 @@
 
 ;;; Code:
 
-(require 'install-packages-pack)
-(install-packages-pack/install-packs '(ediff
-                                       hideshow
-                                       paredit
-                                       fold-dwim
-                                       smartscan
-                                       clojure-mode
-                                       highlight
-                                       eval-sexp-fu))
-
-(require 'hideshow)
-(require 'paredit)
-(require 'fold-dwim)
-(require 'smartscan)
-(require 'clojure-mode)
+(use-package ediff)
+(use-package highlight)
+(use-package eval-sexp-fu)
+(use-package hideshow)
+(use-package paredit)
+(use-package fold-dwim)
+(use-package smartscan)
+(use-package clojure-mode)
+(use-package lisp-mode)
 
 ;; common-lisp setup
 
@@ -28,6 +22,7 @@
 ;; when using the git repository
 ;;(add-to-list 'load-path "~/repo/perso/dot-files/slime")
 ;; (require 'slime-autoloads)
+;; (setq slime-net-coding-system 'utf-8-unix)
 
 ;; add paredit mode to different lisp modes
 (dolist (hook '(emacs-lisp-mode-hook
@@ -44,19 +39,19 @@
               (local-set-key (kbd "C-c s s") 'fold-dwim-show-all)
               (smartscan-mode 1))))
 
-;; (setq slime-net-coding-system 'utf-8-unix)
-
 ;; checking parenthesis at save time
-(add-hook 'after-save-hook 'check-parens nil t)
+(use-package files
+  :config
+  (add-hook 'after-save-hook 'check-parens nil t))
 
-(eval-after-load 'paredit
-  '(progn
-    (define-key paredit-mode-map (kbd "C-w") 'kill-region)
-    (define-key paredit-mode-map (kbd "C-M-h") 'backward-kill-sexp)
-    (define-key paredit-mode-map (kbd "M-s") 'paredit-splice-sexp)
-    (define-key paredit-mode-map (kbd "M-S") 'paredit-split-sexp)
-    (define-key paredit-mode-map (kbd "C-h") 'paredit-backward-delete)
-    (define-key paredit-mode-map (kbd "M-?") nil))) ;; unset the help key
+(use-package paredit
+  :config
+  (define-key paredit-mode-map (kbd "C-w") 'kill-region)
+  (define-key paredit-mode-map (kbd "C-M-h") 'backward-kill-sexp)
+  (define-key paredit-mode-map (kbd "M-s") 'paredit-splice-sexp)
+  (define-key paredit-mode-map (kbd "M-S") 'paredit-split-sexp)
+  (define-key paredit-mode-map (kbd "C-h") 'paredit-backward-delete)
+  (define-key paredit-mode-map (kbd "M-?") nil)) ;; unset the help key
 
 (provide 'lisp-pack)
 ;;; lisp-pack.el ends here
