00723913011ed59d6e906fd23376deaf1558820d
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-09-06 10:53:30 +0200

    Fix slime loading using use-package

--- lisp-pack.el
+++ lisp-pack.el
@@ -13,7 +13,6 @@
 (use-package smartscan)
 (use-package clojure-mode)
 (use-package lisp-mode)
-(use-package slime)
 
 ;; common-lisp setup
 
@@ -51,60 +50,63 @@
 
 ;; Adapted from Dimitri Fontaine's, thanks dude!
 
-(require 'slime)
-
 (defvar *lisp-pack-quicklisp-home* (or (getenv "QUICKLISP_HOME") "~/quicklisp")
   "Quicklisp's home.")
 
-(slime-setup '(slime-fancy))
+(use-package slime
+  :init
+  (remove-hook 'slime-load-hook 'slime-repl-init)
+  :config
+  ;; (slime-setup '(slime-fancy))
 
-(-when-let (quicklisp-slime-helper-file (expand-file-name (format "%s/slime-helper.el" *lisp-pack-quicklisp-home*)))
-  (when (file-exists-p quicklisp-slime-helper-file)
-    (load quicklisp-slime-helper-file)))
+  (-when-let (quicklisp-slime-helper-file (expand-file-name (format "%s/slime-helper.el" *lisp-pack-quicklisp-home*)))
+    (when (file-exists-p quicklisp-slime-helper-file)
+      (load quicklisp-slime-helper-file)))
 
-(custom-set-variables '(slime-net-coding-system 'utf-8-unix))
+  (custom-set-variables '(slime-net-coding-system 'utf-8-unix))
 
-(defun lisp-pack-lookup-lisp-binary (paths)
-  "Lookup PATHS until one binary is found.
+
+  (defun lisp-pack-lookup-lisp-binary (paths)
+    "Lookup PATHS until one binary is found.
 If none is found, the last one is used."
-  (cl-loop for path in paths
-           until (file-exists-p path)
-           finally return path))
+    (cl-loop for path in paths
+             until (file-exists-p path)
+             finally return path))
 
-;; http://common-lisp.net/project/slime/doc/html/Multiple-Lisps.html
-;; discover the installed lisp
-(let* ((sbcl (lisp-pack-lookup-lisp-binary '("/usr/local/bin/sbcl"
-                                             "/usr/bin/sbcl"
-                                             "/run/current-system/sw/bin/sbcl")))
-       (ccl  (lisp-pack-lookup-lisp-binary '("/usr/local/bin/ccl64"
-                                             "/usr/bin/ccl64")))
-       (clisp (lisp-pack-lookup-lisp-binary '("/usr/local/bin/clisp"
-                                              "/usr/bin/clisp")))
-       (ecl   (lisp-pack-lookup-lisp-binary '("/usr/local/bin/ecl"
-                                              "/usr/bin/ecl"))))
+  ;; http://common-lisp.net/project/slime/doc/html/Multiple-Lisps.html
+  ;; discover the installed lisp
+  (let* ((sbcl (lisp-pack-lookup-lisp-binary '("/usr/local/bin/sbcl"
+                                               "/usr/bin/sbcl"
+                                               "/run/current-system/sw/bin/sbcl")))
+         (ccl  (lisp-pack-lookup-lisp-binary '("/usr/local/bin/ccl64"
+                                               "/usr/bin/ccl64")))
+         (clisp (lisp-pack-lookup-lisp-binary '("/usr/local/bin/clisp"
+                                                "/usr/bin/clisp")))
+         (ecl   (lisp-pack-lookup-lisp-binary '("/usr/local/bin/ecl"
+                                                "/usr/bin/ecl"))))
 
-  (setq slime-lisp-implementations `((sbcl (,sbcl)             :coding-system utf-8-unix)
-                                     (ccl  (,ccl "-K" "utf-8") :coding-system utf-8-unix)
-                                     (clisp (,clisp)           :coding-system utf-8-unix)
-                                     (ecl (,ecl)               :coding-system utf-8-unix))
-        slime-default-lisp 'sbcl
-        inferior-lisp-program (caar (assoc-default 'sbcl slime-lisp-implementations))))
+    (setq slime-lisp-implementations `((sbcl (,sbcl)             :coding-system utf-8-unix)
+                                       (ccl  (,ccl "-K" "utf-8") :coding-system utf-8-unix)
+                                       (clisp (,clisp)           :coding-system utf-8-unix)
+                                       (ecl (,ecl)               :coding-system utf-8-unix))
+          slime-default-lisp 'sbcl
+          inferior-lisp-program (caar (assoc-default 'sbcl slime-lisp-implementations))))
 
-;; add path to documentation
-;; (cl-loop for p in '("~/dev/CL/dpans2texi-1.05"
-;;                     "~/dev/CL/cl-yacc"
-;;                     "~/dev/CL/asdf/doc")
-;;          do (add-to-list 'Info-directory-list p))
+  ;; add path to documentation
+  ;; (cl-loop for p in '("~/dev/CL/dpans2texi-1.05"
+  ;;                     "~/dev/CL/cl-yacc"
+  ;;                     "~/dev/CL/asdf/doc")
+  ;;          do (add-to-list 'Info-directory-list p))
 
-(defun lisp-pack-slime-new-repl (&optional new-port)
-  "Create additional REPL for the current Lisp connection.
+  (defun lisp-pack-slime-new-repl (&optional new-port)
+    "Create additional REPL for the current Lisp connection.
 NEW-PORT is optionally the port to change."
-  (interactive)
-  (if (slime-current-connection)
-      (let ((port (or new-port (slime-connection-port (slime-connection)))))
-        (slime-eval `(swank:create-server :port ,port))
-        (slime-connect slime-lisp-host port))
-    (error "Not connected")))
+    (interactive)
+    (if (slime-current-connection)
+        (let ((port (or new-port (slime-connection-port (slime-connection)))))
+          (slime-eval `(swank:create-server :port ,port))
+          (slime-connect slime-lisp-host port))
+      (error "Not connected"))))
 
 ;;
 ;; Fix some SLIME indentation shortcomings.
